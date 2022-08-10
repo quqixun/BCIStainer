25ca07d305c5920920702cf09da74c4828e94bfc
quqixun <quqixun@gmail.com>
2022-08-10 19:14:05 +0800

    start modify starganv2

--- libs/models/starganv2.py
+++ libs/models/starganv2.py
@@ -1,109 +1,22 @@
-"""
+'''
 StarGAN v2
 Copyright (c) 2020-present NAVER Corp.
 This work is licensed under the Creative Commons Attribution-NonCommercial
 4.0 International License. To view a copy of this license, visit
 http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
 Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
-"""
+'''
 
-import copy
+
 import math
-
-# from munch import Munch
-import numpy as np
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
-# from core.wing import FAN
-
-
-# from kornia.filters import filter2d
-# class Blur(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         f = torch.Tensor([1, 2, 1])
-#         self.register_buffer('f', f)
-#     def forward(self, x):
-#         f = self.f
-#         f = f[None, None, :] * f [None, :, None]
-#         return filter2d(x, f, normalized=True)
-
-
-# def exists(val):
-#     return val is not None
-
-
-# class RGBBlock(nn.Module):
-#     def __init__(self, latent_dim, input_channel, upsample, rgba = False):
-#         super().__init__()
-#         self.input_channel = input_channel
-#         self.to_style = nn.Linear(latent_dim, input_channel)
-
-#         out_filters = 3 if not rgba else 4
-#         self.conv = Conv2DMod(input_channel, out_filters, 1, demod=False)
-
-#         self.upsample = nn.Sequential(
-#             nn.Upsample(scale_factor = 2, mode='bilinear', align_corners=False),
-#             Blur()
-#         ) if upsample else None
-
-#     def forward(self, x, prev_rgb, istyle):
-#         b, c, h, w = x.shape
-#         style = self.to_style(istyle)
-#         x = self.conv(x, style)
-
-#         if exists(prev_rgb):
-#             x = x + prev_rgb
-
-#         if exists(self.upsample):
-#             x = self.upsample(x)
-
-#         return x
-
-
-# class Conv2DMod(nn.Module):
-#     def __init__(self, in_chan, out_chan, kernel, demod=True, stride=1, dilation=1, eps = 1e-8, **kwargs):
-#         super().__init__()
-#         self.filters = out_chan
-#         self.demod = demod
-#         self.kernel = kernel
-#         self.stride = stride
-#         self.dilation = dilation
-#         self.weight = nn.Parameter(torch.randn((out_chan, in_chan, kernel, kernel)))
-#         self.eps = eps
-#         nn.init.kaiming_normal_(self.weight, a=0, mode='fan_in', nonlinearity='leaky_relu')
-
-#     def _get_same_padding(self, size, kernel, dilation, stride):
-#         return ((size - 1) * (stride - 1) + dilation * (kernel - 1)) // 2
-
-#     def forward(self, x, y):
-#         b, c, h, w = x.shape
-
-#         w1 = y[:, None, :, None, None]
-#         w2 = self.weight[None, :, :, :, :]
-#         weights = w2 * (w1 + 1)
-
-#         if self.demod:
-#             d = torch.rsqrt((weights ** 2).sum(dim=(2, 3, 4), keepdim=True) + self.eps)
-#             weights = weights * d
-
-#         x = x.reshape(1, -1, h, w)
-
-#         _, _, *ws = weights.shape
-#         weights = weights.reshape(b * self.filters, *ws)
-
-#         padding = self._get_same_padding(h, self.kernel, self.dilation, self.stride)
-#         x = F.conv2d(x, weights, padding=padding, groups=b)
-
-#         x = x.reshape(-1, self.filters, h, w)
-#         return x
-
-
 
 class ResBlk(nn.Module):
-    def __init__(self, dim_in, dim_out, actv=nn.LeakyReLU(0.2),
+    def __init__(self, dim_in, dim_out, actv=nn.LeakyReLU(0.2, True),
                  normalize=False, downsample=False):
         super().__init__()
         self.actv = actv
@@ -120,6 +33,8 @@ class ResBlk(nn.Module):
             self.norm2 = nn.InstanceNorm2d(dim_in, affine=True)
         if self.learned_sc:
             self.conv1x1 = nn.Conv2d(dim_in, dim_out, 1, 1, 0, bias=False)
+        if self.downsample:
+            self.avgpool = nn.AvgPool2d(2)
 
     def _shortcut(self, x):
         if self.learned_sc:
@@ -134,7 +49,7 @@ class ResBlk(nn.Module):
         x = self.actv(x)
         x = self.conv1(x)
         if self.downsample:
-            x = F.avg_pool2d(x, 2)
+            x = self.avgpool(x)
         if self.normalize:
             x = self.norm2(x)
         x = self.actv(x)
@@ -160,10 +75,9 @@ class AdaIN(nn.Module):
 
 
 class AdainResBlk(nn.Module):
-    def __init__(self, dim_in, dim_out, style_dim=64, w_hpf=0,
-                 actv=nn.LeakyReLU(0.2), upsample=False):
+    def __init__(self, dim_in, dim_out, style_dim=64,
+                 actv=nn.LeakyReLU(0.2, True), upsample=False):
         super().__init__()
-        self.w_hpf = w_hpf
         self.actv = actv
         self.upsample = upsample
         self.learned_sc = dim_in != dim_out
@@ -197,192 +111,74 @@ class AdainResBlk(nn.Module):
 
     def forward(self, x, s):
         out = self._residual(x, s)
-        if self.w_hpf == 0:
-            out = (out + self._shortcut(x)) / math.sqrt(2)
-        return out
+        out = out + self._shortcut(x)
+        return out / math.sqrt(2)
 
 
 class HighPass(nn.Module):
-    def __init__(self, w_hpf, device):
+    def __init__(self, w_hpf):
         super(HighPass, self).__init__()
-        self.register_buffer('filter',
-                             torch.tensor([[-1, -1, -1],
-                                           [-1, 8., -1],
-                                           [-1, -1, -1]]) / w_hpf)
+        self.register_buffer(
+            'filter',
+            torch.tensor([[-1, -1, -1], [-1, 8., -1], [-1, -1, -1]]) / w_hpf
+        )
 
     def forward(self, x):
         filter = self.filter.unsqueeze(0).unsqueeze(1).repeat(x.size(1), 1, 1, 1)
         return F.conv2d(x, filter, padding=1, groups=x.size(1))
 
 
-class Generator(nn.Module):
-    def __init__(self, img_size=256, style_dim=64, max_conv_dim=512, w_hpf=1):
-        super().__init__()
-        dim_in = 2**14 // img_size
-        self.img_size = img_size
-        self.from_rgb = nn.Conv2d(3, dim_in, 3, 1, 1)
+class StarAdaGenerator(nn.Module):
+
+    def __init__(self, style_dim=64, n_enc1=3, n_blocks=2, ngf=32):
+        super(StarAdaGenerator, self).__init__()
+        max_conv_dim=512
+
+        self.from_rgb = nn.Conv2d(3, ngf, 3, 1, 1)
         self.encode = nn.ModuleList()
         self.decode = nn.ModuleList()
         self.to_rgb = nn.Sequential(
-            nn.InstanceNorm2d(dim_in, affine=True),
+            nn.InstanceNorm2d(ngf, affine=True),
             nn.LeakyReLU(0.2),
-            nn.Conv2d(dim_in, 3, 1, 1, 0))
+            nn.Conv2d(ngf, 3, 1, 1, 0)
+        )
+
+        self.classifier = None
+        style_dim = self.classifier.style_dim
 
         # down/up-sampling blocks
-        repeat_num = int(np.log2(img_size)) - 4
-        if w_hpf > 0:
-            repeat_num += 1
+        repeat_num = n_enc1
+        dim_in = ngf
         for _ in range(repeat_num):
-            dim_out = min(dim_in*2, max_conv_dim)
-            self.encode.append(
-                ResBlk(dim_in, dim_out, normalize=True, downsample=True))
-            self.decode.insert(
-                0, AdainResBlk(dim_out, dim_in, style_dim,
-                               w_hpf=w_hpf, upsample=True))  # stack-like
+            dim_out = min(dim_in * 2, max_conv_dim)
+            self.encode.append(ResBlk(dim_in, dim_out, normalize=True, downsample=True))
+            self.decode.insert(0, AdainResBlk(dim_out, dim_in, style_dim, upsample=True))  # stack-like
             dim_in = dim_out
 
         # bottleneck blocks
-        for _ in range(2):
-            self.encode.append(
-                ResBlk(dim_out, dim_out, normalize=True))
-            self.decode.insert(
-                0, AdainResBlk(dim_out, dim_out, style_dim, w_hpf=w_hpf))
+        for _ in range(n_blocks):
+            self.encode.append(ResBlk(dim_out, dim_out, normalize=True))
+            self.decode.insert(0, AdainResBlk(dim_out, dim_out, style_dim))
 
-        if w_hpf > 0:
-            device = torch.device(
-                'cuda' if torch.cuda.is_available() else 'cpu')
-            self.hpf = HighPass(w_hpf, device)
+    def forward(self, x):
 
-    def forward(self, x, s, masks=None):
+        level, style = self.classifier(x)
         x = self.from_rgb(x)
-        cache = {}
         for block in self.encode:
-            if (masks is not None) and (x.size(2) in [32, 64, 128]):
-                cache[x.size(2)] = x
             x = block(x)
         for block in self.decode:
-            x = block(x, s)
-            if (masks is not None) and (x.size(2) in [32, 64, 128]):
-                mask = masks[0] if x.size(2) in [32] else masks[1]
-                mask = F.interpolate(mask, size=x.size(2), mode='bilinear')
-                x = x + self.hpf(mask * cache[x.size(2)])
+            x = block(x, style)
         return self.to_rgb(x)
 
 
-# class MappingNetwork(nn.Module):
-#     def __init__(self, latent_dim=16, style_dim=64, num_domains=2):
-#         super().__init__()
-#         layers = []
-#         layers += [nn.Linear(latent_dim, 512)]
-#         layers += [nn.ReLU()]
-#         for _ in range(3):
-#             layers += [nn.Linear(512, 512)]
-#             layers += [nn.ReLU()]
-#         self.shared = nn.Sequential(*layers)
+if __name__ == '__main__':
 
-#         self.unshared = nn.ModuleList()
-#         for _ in range(num_domains):
-#             self.unshared += [nn.Sequential(nn.Linear(512, 512),
-#                                             nn.ReLU(),
-#                                             nn.Linear(512, 512),
-#                                             nn.ReLU(),
-#                                             nn.Linear(512, 512),
-#                                             nn.ReLU(),
-#                                             nn.Linear(512, style_dim))]
+    x = torch.rand(1, 3, 1024, 1024).cuda()
+    style = torch.rand(1, 512).cuda()
 
-#     def forward(self, z, y):
-#         h = self.shared(z)
-#         out = []
-#         for layer in self.unshared:
-#             out += [layer(h)]
-#         out = torch.stack(out, dim=1)  # (batch, num_domains, style_dim)
-#         idx = torch.LongTensor(range(y.size(0))).to(y.device)
-#         s = out[idx, y]  # (batch, style_dim)
-#         return s
+    model = StarAdaGenerator(style_dim=512, max_conv_dim=512)
+    model.cuda()
+    # print(model)
 
-
-# class StyleEncoder(nn.Module):
-#     def __init__(self, img_size=256, style_dim=64, num_domains=2, max_conv_dim=512):
-#         super().__init__()
-#         dim_in = 2**14 // img_size
-#         blocks = []
-#         blocks += [nn.Conv2d(3, dim_in, 3, 1, 1)]
-
-#         repeat_num = int(np.log2(img_size)) - 2
-#         for _ in range(repeat_num):
-#             dim_out = min(dim_in*2, max_conv_dim)
-#             blocks += [ResBlk(dim_in, dim_out, downsample=True)]
-#             dim_in = dim_out
-
-#         blocks += [nn.LeakyReLU(0.2)]
-#         blocks += [nn.Conv2d(dim_out, dim_out, 4, 1, 0)]
-#         blocks += [nn.LeakyReLU(0.2)]
-#         self.shared = nn.Sequential(*blocks)
-
-#         self.unshared = nn.ModuleList()
-#         for _ in range(num_domains):
-#             self.unshared += [nn.Linear(dim_out, style_dim)]
-
-#     def forward(self, x, y):
-#         h = self.shared(x)
-#         h = h.view(h.size(0), -1)
-#         out = []
-#         for layer in self.unshared:
-#             out += [layer(h)]
-#         out = torch.stack(out, dim=1)  # (batch, num_domains, style_dim)
-#         idx = torch.LongTensor(range(y.size(0))).to(y.device)
-#         s = out[idx, y]  # (batch, style_dim)
-#         return s
-
-
-# class Discriminator(nn.Module):
-#     def __init__(self, img_size=256, num_domains=2, max_conv_dim=512):
-#         super().__init__()
-#         dim_in = 2**14 // img_size
-#         blocks = []
-#         blocks += [nn.Conv2d(3, dim_in, 3, 1, 1)]
-
-#         repeat_num = int(np.log2(img_size)) - 2
-#         for _ in range(repeat_num):
-#             dim_out = min(dim_in*2, max_conv_dim)
-#             blocks += [ResBlk(dim_in, dim_out, downsample=True)]
-#             dim_in = dim_out
-
-#         blocks += [nn.LeakyReLU(0.2)]
-#         blocks += [nn.Conv2d(dim_out, dim_out, 4, 1, 0)]
-#         blocks += [nn.LeakyReLU(0.2)]
-#         blocks += [nn.Conv2d(dim_out, num_domains, 1, 1, 0)]
-#         self.main = nn.Sequential(*blocks)
-
-#     def forward(self, x, y):
-#         out = self.main(x)
-#         out = out.view(out.size(0), -1)  # (batch, num_domains)
-#         idx = torch.LongTensor(range(y.size(0))).to(y.device)
-#         out = out[idx, y]  # (batch)
-#         return out
-
-
-# def build_model(args):
-#     generator = nn.DataParallel(Generator(args.img_size, args.style_dim, w_hpf=args.w_hpf))
-#     mapping_network = nn.DataParallel(MappingNetwork(args.latent_dim, args.style_dim, args.num_domains))
-#     style_encoder = nn.DataParallel(StyleEncoder(args.img_size, args.style_dim, args.num_domains))
-#     discriminator = nn.DataParallel(Discriminator(args.img_size, args.num_domains))
-#     generator_ema = copy.deepcopy(generator)
-#     mapping_network_ema = copy.deepcopy(mapping_network)
-#     style_encoder_ema = copy.deepcopy(style_encoder)
-
-#     nets = Munch(generator=generator,
-#                  mapping_network=mapping_network,
-#                  style_encoder=style_encoder,
-#                  discriminator=discriminator)
-#     nets_ema = Munch(generator=generator_ema,
-#                      mapping_network=mapping_network_ema,
-#                      style_encoder=style_encoder_ema)
-
-#     if args.w_hpf > 0:
-#         fan = nn.DataParallel(FAN(fname_pretrained=args.wing_path).eval())
-#         fan.get_heatmap = fan.module.get_heatmap
-#         nets.fan = fan
-#         nets_ema.fan = fan
-
-#     return nets,
+    output = model(x)
+    print(output.size())
