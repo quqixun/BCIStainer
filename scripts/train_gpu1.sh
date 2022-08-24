0423e10c3deb4993152120e5c20c6994d33aeb1c
quqixun <quqixun@gmail.com>
2022-08-24 18:55:37 +0800

    more exps

--- scripts/train_gpu1.sh
+++ scripts/train_gpu1.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 
-config_file_list=(
-    ./configs/style_translator/exp2.yaml
-)
+# config_file_list=(
+#     ./configs/style_translator/exp2.yaml
+# )
 
-for config_file in ${config_file_list[@]}; do
+# for config_file in ${config_file_list[@]}; do
 
-    CUDA_VISIBLE_DEVICES=1          \
-    python train.py                 \
-        --train_dir   ./data/train  \
-        --val_dir     ./data/val    \
-        --exp_root    ./experiments \
-        --config_file $config_file  \
-        --trainer     basic
+#     CUDA_VISIBLE_DEVICES=1          \
+#     python train.py                 \
+#         --train_dir   ./data/train  \
+#         --val_dir     ./data/val    \
+#         --exp_root    ./experiments \
+#         --config_file $config_file  \
+#         --trainer     basic
 
-done
+# done
 
 
 config_file_list=(
--- scripts/train_gpu2.sh
+++ scripts/train_gpu2.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 
-config_file_list=(
-    ./configs/style_translator/exp3.yaml
-)
+# config_file_list=(
+#     ./configs/style_translator/exp3.yaml
+# )
 
-for config_file in ${config_file_list[@]}; do
+# for config_file in ${config_file_list[@]}; do
 
-    CUDA_VISIBLE_DEVICES=2          \
-    python train.py                 \
-        --train_dir   ./data/train  \
-        --val_dir     ./data/val    \
-        --exp_root    ./experiments \
-        --config_file $config_file  \
-        --trainer     basic
+#     CUDA_VISIBLE_DEVICES=2          \
+#     python train.py                 \
+#         --train_dir   ./data/train  \
+#         --val_dir     ./data/val    \
+#         --exp_root    ./experiments \
+#         --config_file $config_file  \
+#         --trainer     basic
 
-done
+# done
 
 
 config_file_list=(
