5fd08552692a99dcb2adacf96ff0ae73d1dad03d
quqixun <quqixun@gmail.com>
2022-09-21 15:31:55 +0800

    new exps

---
+++ scripts/eval_gpu2.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+
+model_name=model_best_psnr
+apply_tta_list=(false true)
+
+
+evaluator=basic
+config_file_list=(
+    # ./configs/style_translator/exp4.yaml
+    # ./configs/style_translator/exp5.yaml
+    # ./configs/style_translator/exp6.yaml
+    ./configs/style_translator/exp7.yaml
+    ./configs/style_translator/exp8.yaml
+    # ./configs/style_translator_cmp/exp1.yaml
+    # ./configs/style_translator_cmp/exp2.yaml
+    # ./configs/style_translator_cmp/exp3.yaml
+    # ./configs/style_translator_cmp/exp4.yaml
+    ./configs/style_translator_cmp/exp5.yaml
+    ./configs/style_translator_cmp/exp6.yaml
+)
+
+for config_file in ${config_file_list[@]}; do
+for apply_tta in ${apply_tta_list[@]}; do
+
+    CUDA_VISIBLE_DEVICES=2            \
+    python evaluate.py                \
+        --data_dir    ./data/test     \
+        --exp_root    ./experiments   \
+        --output_root ./evaluations   \
+        --config_file $config_file    \
+        --model_name  $model_name     \
+        --apply_tta   $apply_tta      \
+        --evaluator   $evaluator
+
+done; done
+
+
+evaluator=cahr
+config_file_list=(
+    # ./configs/style_translator_cahr/exp6.yaml
+    # ./configs/style_translator_cahr/exp9.yaml
+    # ./configs/style_translator_cahr/exp13.yaml
+    # ./configs/style_translator_cahr/exp14.yaml
+    ./configs/style_translator_cahr/exp15.yaml
+    ./configs/style_translator_cahr/exp16.yaml
+    ./configs/style_translator_cahr/exp17.yaml
+    ./configs/style_translator_cahr/exp18.yaml
+    ./configs/style_translator_cahr_cmp/exp9.yaml
+    ./configs/style_translator_cahr_cmp/exp10.yaml
+    ./configs/style_translator_cahr_cmp/exp11.yaml
+    ./configs/style_translator_cahr_cmp/exp12.yaml
+)
+
+for config_file in ${config_file_list[@]}; do
+for apply_tta in ${apply_tta_list[@]}; do
+
+    CUDA_VISIBLE_DEVICES=2            \
+    python evaluate.py                \
+        --data_dir    ./data/test     \
+        --exp_root    ./experiments   \
+        --output_root ./evaluations   \
+        --config_file $config_file    \
+        --model_name  $model_name     \
+        --apply_tta   $apply_tta      \
+        --evaluator   $evaluator
+
+done; done
---
+++ scripts/train_gpu3.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+trainer=cahr
+config_file_list=(
+    # ./configs/style_translator_cahr/exp6.yaml
+    # ./configs/style_translator_cahr/exp9.yaml
+    # ./configs/style_translator_cahr/exp13.yaml
+    # ./configs/style_translator_cahr/exp14.yaml
+    # ./configs/style_translator_cahr_cmp/exp1.yaml
+    # ./configs/style_translator_cahr_cmp/exp2.yaml
+    # ./configs/style_translator_cahr_cmp/exp3.yaml
+    # ./configs/style_translator_cahr_cmp/exp4.yaml
+    # ./configs/style_translator_cahr_cmp/exp5.yaml
+    # ./configs/style_translator_cahr_cmp/exp6.yaml
+    # ./configs/style_translator_cahr_cmp/exp7.yaml
+    # ./configs/style_translator_cahr_cmp/exp8.yaml
+    ./configs/style_translator_ocahr/exp6.yaml
+    ./configs/style_translator_ocahr/exp9.yaml
+    ./configs/style_translator_ocahr/exp13.yaml
+    ./configs/style_translator_ocahr/exp14.yaml
+    ./configs/style_translator_ocahr_cmp/exp1.yaml
+    ./configs/style_translator_ocahr_cmp/exp2.yaml
+    ./configs/style_translator_ocahr_cmp/exp3.yaml
+    ./configs/style_translator_ocahr_cmp/exp4.yaml
+    ./configs/style_translator_ocahr_cmp/exp5.yaml
+    ./configs/style_translator_ocahr_cmp/exp6.yaml
+    ./configs/style_translator_ocahr_cmp/exp7.yaml
+    ./configs/style_translator_ocahr_cmp/exp8.yaml
+)
+
+for config_file in ${config_file_list[@]}; do
+
+    CUDA_VISIBLE_DEVICES=3          \
+    python train.py                 \
+        --train_dir   ./data/train  \
+        --val_dir     ./data/val    \
+        --exp_root    ./experiments \
+        --config_file $config_file  \
+        --trainer     $trainer
+
+done
