01bd661595a517a58f70998037f92b4bc7589f95
quqixun <quqixun@gmail.com>
2022-09-22 19:25:54 +0800

    disable ocahr exps, start new exps

--- scripts/train_gpu2.sh
+++ scripts/train_gpu2.sh
@@ -6,10 +6,16 @@ config_file_list=(
     # ./configs/style_translator/exp4.yaml
     # ./configs/style_translator/exp5.yaml
     # ./configs/style_translator/exp6.yaml
+    ./configs/style_translator/exp7.yaml
+    ./configs/style_translator/exp8.yaml
     # ./configs/style_translator_cmp/exp1.yaml
     # ./configs/style_translator_cmp/exp2.yaml
     # ./configs/style_translator_cmp/exp3.yaml
     # ./configs/style_translator_cmp/exp4.yaml
+    ./configs/style_translator_cmp/exp5.yaml
+    ./configs/style_translator_cmp/exp6.yaml
+    ./configs/style_translator_cmp/exp7.yaml
+    ./configs/style_translator_cmp/exp8.yaml
 )
 
 for config_file in ${config_file_list[@]}; do
@@ -31,14 +37,16 @@ config_file_list=(
     # ./configs/style_translator_cahr/exp9.yaml
     # ./configs/style_translator_cahr/exp13.yaml
     # ./configs/style_translator_cahr/exp14.yaml
+    # ./configs/style_translator_cahr_cmp/exp1.yaml
+    # ./configs/style_translator_cahr_cmp/exp2.yaml
+    # ./configs/style_translator_cahr_cmp/exp3.yaml
+    # ./configs/style_translator_cahr_cmp/exp4.yaml
+    # ./configs/style_translator_cahr_cmp/exp5.yaml
+    # ./configs/style_translator_cahr_cmp/exp6.yaml
+    # ./configs/style_translator_cahr_cmp/exp7.yaml
+    # ./configs/style_translator_cahr_cmp/exp8.yaml
     ./configs/style_translator_cahr/exp15.yaml
     ./configs/style_translator_cahr/exp16.yaml
-    ./configs/style_translator_cahr/exp17.yaml
-    ./configs/style_translator_cahr/exp18.yaml
-    ./configs/style_translator_cahr_cmp/exp9.yaml
-    ./configs/style_translator_cahr_cmp/exp10.yaml
-    ./configs/style_translator_cahr_cmp/exp11.yaml
-    ./configs/style_translator_cahr_cmp/exp12.yaml
 )
 
 for config_file in ${config_file_list[@]}; do
