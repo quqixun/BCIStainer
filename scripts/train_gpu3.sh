fb0caa0497dafe3520d4090fd16fdf7596901438
quqixun <quqixun@gmail.com>
2022-09-23 19:27:50 +0800

    exps using lpips

--- scripts/train_gpu3.sh
+++ scripts/train_gpu3.sh
@@ -15,24 +15,10 @@ config_file_list=(
     # ./configs/style_translator_cahr_cmp/exp6.yaml
     # ./configs/style_translator_cahr_cmp/exp7.yaml
     # ./configs/style_translator_cahr_cmp/exp8.yaml
-    # ./configs/style_translator_ocahr/exp6.yaml
-    # ./configs/style_translator_ocahr/exp9.yaml
-    # ./configs/style_translator_ocahr/exp13.yaml
-    # ./configs/style_translator_ocahr/exp14.yaml
-    # ./configs/style_translator_ocahr_cmp/exp1.yaml
-    # ./configs/style_translator_ocahr_cmp/exp2.yaml
-    # ./configs/style_translator_ocahr_cmp/exp3.yaml
-    # ./configs/style_translator_ocahr_cmp/exp4.yaml
-    # ./configs/style_translator_ocahr_cmp/exp5.yaml
-    # ./configs/style_translator_ocahr_cmp/exp6.yaml
-    # ./configs/style_translator_ocahr_cmp/exp7.yaml
-    # ./configs/style_translator_ocahr_cmp/exp8.yaml
     ./configs/style_translator_cahr/exp17.yaml
     ./configs/style_translator_cahr/exp18.yaml
     ./configs/style_translator_cahr_cmp/exp9.yaml
     ./configs/style_translator_cahr_cmp/exp10.yaml
-    ./configs/style_translator_cahr_cmp/exp11.yaml
-    ./configs/style_translator_cahr_cmp/exp12.yaml
 )
 
 for config_file in ${config_file_list[@]}; do
--- scripts/train_local.sh
+++ scripts/train_local.sh
@@ -6,6 +6,7 @@ config_file_list=(
     # ./configs/style_translator/exp4.yaml
     # ./configs/style_translator/exp5.yaml
     # ./configs/style_translator/exp6.yaml
+    ./configs/style_translator/exp7.yaml
     # ./configs/style_translator_cmp/exp1.yaml
     # ./configs/style_translator_cmp/exp2.yaml
     # ./configs/style_translator_cmp/exp3.yaml
@@ -14,7 +15,7 @@ config_file_list=(
 
 for config_file in ${config_file_list[@]}; do
 
-    CUDA_VISIBLE_DEVICES=2          \
+    CUDA_VISIBLE_DEVICES=0          \
     python train.py                 \
         --train_dir   ./data/train  \
         --val_dir     ./data/val    \
@@ -39,15 +40,15 @@ config_file_list=(
     # ./configs/style_translator_cahr_cmp/exp6.yaml
     # ./configs/style_translator_cahr_cmp/exp7.yaml
     # ./configs/style_translator_cahr_cmp/exp8.yaml
-    ./configs/style_translator_cahr/exp15.yaml
-    ./configs/style_translator_cahr/exp16.yaml
-    ./configs/style_translator_cahr_cmp/exp11.yaml
-    ./configs/style_translator_cahr_cmp/exp12.yaml
+    # ./configs/style_translator_cahr/exp15.yaml
+    # ./configs/style_translator_cahr/exp16.yaml
+    # ./configs/style_translator_cahr_cmp/exp11.yaml
+    # ./configs/style_translator_cahr_cmp/exp12.yaml
 )
 
 for config_file in ${config_file_list[@]}; do
 
-    CUDA_VISIBLE_DEVICES=2          \
+    CUDA_VISIBLE_DEVICES=0          \
     python train.py                 \
         --train_dir   ./data/train  \
         --val_dir     ./data/val    \
